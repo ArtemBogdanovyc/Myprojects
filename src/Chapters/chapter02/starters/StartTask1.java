66b414b07494c0b4df181978dd55984b0e3c250f
Artem <kainqlk@gmail.com>
2020-12-08 16:19:34 +0300

    update

---
+++ src/Chapters/chapter02/starters/StartTask1.java
@@ -0,0 +1,15 @@
+package Chapters.chapter02.starters;
+
+
+
+import Chapters.chapter02.tasks.task1.WorkRithString;
+
+public class StartTask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WorkRithString wrkS = new WorkRithString(5);
+wrkS.getStrings();
+
+	}
+}
---
+++ src/Chapters/chapter02/tasks/task1/WorkRithString.java
@@ -0,0 +1,33 @@
+package Chapters.chapter02.tasks.task1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WorkRithString {
+private int quantity;
+private String[] strings;
+Scanner scan;
+private int indMax=0;
+private int indMin=0;
+public WorkRithString(int quantity) {
+	this.quantity=quantity;
+	this.indMax=indMax;
+	this.indMin=indMin;
+	strings=new String[quantity];
+	scan= new Scanner(System.in);
+}
+public void getStrings() {
+	for (int i=0; i<strings.length ; i++) {
+		strings[i]=scan.next();
+		if(strings[i].length()<strings[indMin].length()) {
+			indMin=i;
+		}else if(strings[i].length()> strings[indMax].length()) {
+			indMax=i;
+		}
+	}
+	System.out.println( strings[indMin]);
+	System.out.println( strings[indMax]);
+	System.out.println( strings[indMin].length());
+	System.out.println( strings[indMax].length());
+	}
+}
